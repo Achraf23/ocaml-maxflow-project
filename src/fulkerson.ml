a4ddca50fac915e3f9ee6342ac2cd92f1ae8c9a6
Achraf <achrafbensebaa23@gmail.com>
2023-12-11 21:08:30 +0100

    difference graph

--- src/fulkerson.ml
+++ src/fulkerson.ml
@@ -1,12 +1,6 @@
 open Graph
 open Tools
 
-let test graph =
-  let arcs = out_arcs graph 1 in
-  match arcs with
-  | [] -> clone_nodes graph
-  | x::_rest -> new_arc (clone_nodes graph) x 
-
 
 let successors n graph = 
   let successors = out_arcs graph n in
@@ -49,6 +43,30 @@ let rec run_ford_fulkerson graph flow_graph src tgt =
     Printf.printf "Flow: %d\n" new_flow;
     let updated_flow_graph = update_flow_graph flow_graph (fun arc -> check_if_arc_is_in_path arc path) (fun arc -> check_if_backward_arc_is_in_path arc path) new_flow in
     run_ford_fulkerson graph updated_flow_graph src tgt
+
+
+let difference origin_graph flow_graph = 
+  let build = clone_nodes origin_graph in
+  e_fold origin_graph 
+  (fun graph arc -> 
+    (*find matching arc in flow grah*)
+    let arc_f = find_arc flow_graph arc.src arc.tgt in
+    match arc_f with
+    | Some arc_flow -> 
+      let diff = arc.lbl- arc_flow.lbl in
+        (match diff with
+        (*calculate weight difference between original graph and flow graph*)
+        | 0 -> new_arc graph {src=arc.tgt; tgt=arc.src; lbl=arc.lbl}
+        (*diff=0 --> forward arc*)
+        | w when w=arc.lbl -> new_arc graph {src=arc.src; tgt=arc.tgt; lbl=w}
+        (*2 flow arcs if flow val < capacity and not equal to zero*)
+        | _ -> let ajout = new_arc graph {src=arc.src; tgt=arc.tgt; lbl=diff} in
+        new_arc ajout {src=arc.tgt; tgt=arc.src; lbl=arc_flow.lbl})
+    | None -> graph 
+    ) 
+    build
+
+
 
   
 
